e2ac096a784a87b31a010e9d73ae7d21e818bf80
Kapil Whaval <whavalkapil@gmail.com>
2024-08-31 20:02:05 +0530

    fix(swimato-ui): updated base url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const getBaseUrl = (environment: string) => {
-    return environment === "development" ? "http://localhost:8000" : "https://food-delivery-server-s65z.onrender.com"
+    return environment === "development" ? "http://localhost:8000" : "https://server.klickraft.com/swimato"
 };
 export const methods = {
     GET: "GET",
