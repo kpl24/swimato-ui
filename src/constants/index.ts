dceffd4e429c43cdbe7605b352f9a4d8929de527
Kapil Whaval <whavalkapil@gmail.com>
2024-11-07 15:35:55 +0530

    feat(swimato-ui): changed base url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const getBaseUrl = (environment: string) => {
-    return environment === "development" ? "http://localhost:8001" : "https://server.klickraft.com/swimato"
+    return environment === "development" ? "http://localhost:8001" : "https://swimato.onrender.com"
 };
 export const methods = {
     GET: "GET",
